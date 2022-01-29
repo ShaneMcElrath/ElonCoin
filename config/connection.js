ba00bad1912cdf31e6fbdfe4958f65289e9fcdca
Shane McElrath <shane@rangermac.com>
2022-01-29 12:06:34 -0500

    Added SetInterval for getting tweets for stock data. With Ronny, Connor, Nelson, and Shane

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
+
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+require('./jobs/fetchTweets.js');
+require('./jobs/fetchStocks.js')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
