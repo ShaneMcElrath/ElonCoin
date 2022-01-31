c2c04d500ab16c54ff23c7a2a3703132f618545f
Shane McElrath <shane@rangermac.com>
2022-01-31 18:50:18 -0500

    hard coded url

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-require('./jobs/fetchTweets');
-require('./jobs/fetchStocks');
+
 
 
 const app = express();
